e958046412ab609a1b8a4a87aadc08be7ece229a
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2023-10-12 13:35:31 -0500

    init

---
+++ lint-staged.config.js
@@ -0,0 +1,20 @@
+const eslintCmd = `cross-env TIMING=1 eslint --quiet --ext .js,.jsx,.ts,.tsx --fix --cache --cache-strategy content`;
+const prettierCmd = `prettier --ignore-unknown --write --cache`;
+
+// TODO globally run commands should `git add .` BEFORE command brings back stash https://github.com/okonet/lint-staged/issues/1253
+// Until that issue is resolved, we'll always --no-stash and just stash/unstash and add everything on our own
+
+/**
+ * @type {import('lint-staged').Config}
+ */
+const config = {
+  "*.{gif,jpeg,jpg,png,svg}": ["imagemin-lint-staged"],
+  "*.{js,jsx,ts,tsx}": [eslintCmd],
+  "*": [prettierCmd],
+  "{.eslint*,package.json}": () => [`${eslintCmd} .`, "git add ."],
+  "{.prettier*,package.json}": () => [`${prettierCmd} .`, "git add ."],
+  "{.env*,.gitattributes}": (files) =>
+    files.map((file) => `sort -o ${file} ${file}`),
+};
+
+module.exports = config;
